5490f16036ab117eda5681fef8ab125a7c199d4e
Jander Reis <jrs_1990@hotmail.com>
2018-08-31 22:25:28 -0300

    aprendizado utilizando o pipe e várias funcionalidades legais

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,36 @@
 var gulp  =require('gulp');
+// var jshint = require('gulp-jshint');
+// var jscs = require('gulp-jscs');
+// var util = require('gulp-util');
+ var print = require('gulp-print').default;
+// var gulpif = require('gulp-if');
+var args = require('yargs').argv;
 
-gulp.task('hello-world', function () {
-    console.log('primeira task usando gulp!')
+var config = require('./gulp.config')();
+var $ = require('gulp-load-plugins')({lazy:true});
+
+
+gulp.task('vet', function () {
+    log('analisando');
+    gulp.src(config.alljs)
+        .pipe($.if(args.jander, print()) )
+        .pipe($.jscs())
+        .pipe($.jshint())
+        .pipe($.jshint.reporter('jshint-stylish',{verbose: true}))
+        .pipe($.jshint.reporter('fail'));
   });
+
+  function log(msg){
+    if (typeof(msg) === 'object')
+    {
+        for(var item in msg){
+            if(msg.hasOwnProperty(item))
+            {
+                $.util.log($.util.colors.blue(msg[item]));
+            }
+        }
+    }
+    else{
+        $.util.log($.util.colors.blue(msg));
+    }
+  }
