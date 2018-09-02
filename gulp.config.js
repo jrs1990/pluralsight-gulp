e797e8436f2c4401036494d2af404019ad7b7bb2
Jander Reis <jrs_1990@hotmail.com>
2018-09-02 10:41:22 -0300

    nda

--- gulp.config.js
+++ gulp.config.js
@@ -1,11 +1,35 @@
 module.exports = function () { 
     var client = './src/client/';
+    var clientApp = client + 'app';
     
     var config = {
         temp: './.tmp',
         alljs : ['./src/**/*.js','./*.js'],
-        less: client + 'styles/styles.less'
+        less: client + 'styles/styles.less',
+        index: client + 'index.html',
+        js: [
+            clientApp + '**/*.module.js',
+            clientApp + '**/*.js',
+            '!' + clientApp + '**/*.spec.js',
+        ],
+        client : client,
+        
+        bower: {
+            json: require('./bower.json'),
+            directory: '.bower_components/',
+            ignorePath: '../..'
+        }
     };
+
+    config.getWiredepDefaultOptions = function () { 
+        var options = {
+            bowerJson: config.bower.json,
+            directory: config.bower.directory,
+            ignorePath: config.bower.ignorePath
+        };
+
+        return options;
+     };
 
 
 
